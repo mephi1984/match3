c950b05bab7d4a38c18174bc2d9c91a5aa8903ab
Vladislav Khorev <mephi1984@gmail.com>
2024-03-04 08:24:38 +0300

    Major update

--- app/src/main/java/com/example/match3/MainActivity.kt
+++ app/src/main/java/com/example/match3/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.match3
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -16,8 +18,45 @@ import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
 
-    val chipImageMatrix = Array(8) { Array<ImageView?>(7) { null } }
+    inner class AnimationSequence(private val imageView: ImageView, private vararg val animations: Animation) : Animation.AnimationListener {
+        private var currentAnimationIndex = 0
 
+        init {
+            animations.forEach { it.setAnimationListener(this) }
+        }
+
+        fun start() {
+            imageView.startAnimation(animations[currentAnimationIndex])
+        }
+
+        override fun onAnimationStart(animation: Animation?) {
+            // Начало анимации
+        }
+
+        override fun onAnimationEnd(animation: Animation?) {
+            currentAnimationIndex++
+            if (currentAnimationIndex < animations.size) {
+                // Переходим к следующей анимации
+                animations[currentAnimationIndex].setAnimationListener(this)
+                imageView.startAnimation(animations[currentAnimationIndex])
+            } else {
+                // Все анимации завершены
+            }
+        }
+
+        override fun onAnimationRepeat(animation: Animation?) {
+            // Повторение анимации
+        }
+    }
+
+    val rows = 8
+    val columns = 7
+    val chipImageMatrix = Array(rows) { Array<ImageView?>(columns) { null } }
+
+    val array = Array(rows) { Array(columns) { Random.nextInt(1, 5) } }
+
+    var currentX = -1
+    var currentY = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -87,67 +126,359 @@ class MainActivity : ComponentActivity() {
         chipImageMatrix[7][6] = this.findViewById<ImageView?>(R.id.imageView8_7)
 
         setListeners()
+        updateImages()
     }
 
-    fun setListeners(){
-        val rows = 8
-        val columns = 7
-        val array = Array(rows) { Array(columns) { Random.nextInt(1, 5) } }
-        val x = 0
-        val y = 0
-        for (x in 0 until 8) {
-            for (y in 0 until 7) {
-                val imageView = chipImageMatrix[x][y] ?: continue
-                when (array[x][y]) {
+    fun updateImages() {
+        for (y in 0 until rows) {
+            for (x in 0 until columns) {
+                val imageView = chipImageMatrix[y][x] ?: continue
+                when (array[y][x]) {
                     1 -> imageView.setImageResource(R.drawable.chip1)
                     2 -> imageView.setImageResource(R.drawable.chip2)
                     3 -> imageView.setImageResource(R.drawable.chip3)
                     4 -> imageView.setImageResource(R.drawable.chip4)
-                    else -> imageView.setImageResource(R.drawable.ic_launcher_background)
-                }
-
-                imageView.setOnClickListener {
-                    if(x==currentX+1 && y==currentY)
-                    {
-                        var c:Int
-                        c = array[x][y]
-                        array[x][y]=array[currentX][currentY]
-                        array[currentX][currentY]=c
-
-                        val imageView = chipImageMatrix[x][y]
-                        when (array[x][y]) {
-                            1 -> imageView?.setImageResource(R.drawable.chip1)
-                            2 -> imageView?.setImageResource(R.drawable.chip2)
-                            3 -> imageView?.setImageResource(R.drawable.chip3)
-                            4 -> imageView?.setImageResource(R.drawable.chip4)
-                            else -> imageView?.setImageResource(R.drawable.ic_launcher_background)
-                        }
-                        val imageView1 = chipImageMatrix[currentX][currentY]
-                        when (array[currentX][currentY]) {
-                            1 -> imageView1?.setImageResource(R.drawable.chip1)
-                            2 -> imageView1?.setImageResource(R.drawable.chip2)
-                            3 -> imageView1?.setImageResource(R.drawable.chip3)
-                            4 -> imageView1?.setImageResource(R.drawable.chip4)
-                            else -> imageView?.setImageResource(R.drawable.ic_launcher_background)
-                        }
-
-
-                    }
-                    else{
-                        currentX=x
-                        currentY=y
-                    }
-                    currentX = x
-                    currentY = y
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Нажали на ImageView, $x, $y",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    else -> imageView.setImageResource(R.drawable.empty)
                 }
             }
         }
     }
-    var currentX = 0
-    var currentY = 0
+
+    fun swap(x1: Int, y1: Int, x2:Int, y2:Int)
+    {
+        var c:Int
+        c = array[y1][x1]
+        array[y1][x1]=array[y2][x2]
+        array[y2][x2]=c
+    }
+
+    fun solveMatches()
+    {
+        
+    }
+
+    fun startAnimationLeftRight(x1: Int, y1: Int, x2: Int, y2: Int)
+    {
+        val imageView1 = chipImageMatrix[y1][x1]
+
+        val translateAnimation1 = TranslateAnimation(
+            0.0f,
+            -150.0f,
+            0.0f,
+            0.0f
+        )
+        translateAnimation1.duration = 200
+
+        translateAnimation1.startOffset = 0
+
+        translateAnimation1.repeatCount = 1
+
+        translateAnimation1.interpolator = android.view.animation.LinearInterpolator()
+
+        translateAnimation1.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                animation?.cancel()
+                swap(x1,y1, x2, y2)
+                updateImages()
+            }
+        })
+
+        imageView1?.startAnimation(translateAnimation1)
+
+        val imageView2 = chipImageMatrix[y2][x2]
+
+        val translateAnimation2 = TranslateAnimation(
+            0.0f,
+            150.0f,
+            0.0f,
+            0.0f
+        )
+        translateAnimation2.duration = 200
+
+        translateAnimation2.startOffset = 0
+
+        translateAnimation2.repeatCount = 1
+
+        translateAnimation2.interpolator = android.view.animation.LinearInterpolator()
+
+        translateAnimation2.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                animation?.cancel()
+            }
+        })
+
+        imageView2?.startAnimation(translateAnimation2)
+
+    }
+
+
+    fun startAnimationLeftRightFail(x1: Int, y1: Int, x2: Int, y2: Int)
+    {
+        val imageView1 = chipImageMatrix[y1][x1]
+
+        val translateAnimation1 = TranslateAnimation(
+            0.0f,
+            -150.0f,
+            0.0f,
+            0.0f
+        )
+        translateAnimation1.duration = 200
+
+        translateAnimation1.startOffset = 0
+
+        translateAnimation1.repeatCount = 0
+
+        translateAnimation1.interpolator = android.view.animation.LinearInterpolator()
+
+        val translateAnimation1r = TranslateAnimation(
+            -150.0f,
+            0.0f,
+            0.0f,
+            0.0f
+        )
+        translateAnimation1r.duration = 200
+
+        translateAnimation1r.startOffset = 0
+
+        translateAnimation1r.repeatCount = 0
+
+        translateAnimation1r.interpolator = android.view.animation.LinearInterpolator()
+
+        val animationSequence1 = AnimationSequence(imageView1!!, translateAnimation1, translateAnimation1r)
+
+        animationSequence1.start()
+
+        val imageView2 = chipImageMatrix[y2][x2]
+
+        val translateAnimation2 = TranslateAnimation(
+            0.0f,
+            150.0f,
+            0.0f,
+            0.0f
+        )
+        translateAnimation2.duration = 200
+
+        translateAnimation2.startOffset = 0
+
+        translateAnimation2.repeatCount = 0
+
+        translateAnimation2.interpolator = android.view.animation.LinearInterpolator()
+
+        val translateAnimation2r = TranslateAnimation(
+            150.0f,
+            0.0f,
+            0.0f,
+            0.0f
+        )
+        translateAnimation2r.duration = 200
+
+        translateAnimation2r.startOffset = 0
+
+        translateAnimation2r.repeatCount = 0
+
+        translateAnimation2r.interpolator = android.view.animation.LinearInterpolator()
+
+        val animationSequence2 = AnimationSequence(imageView2!!, translateAnimation2, translateAnimation2r)
+
+        animationSequence2.start()
+
+    }
+
+
+    fun startAnimationTopBottomFail(x1: Int, y1: Int, x2: Int, y2: Int)
+    {
+        val imageView1 = chipImageMatrix[y1][x1]
+
+        val translateAnimation1 = TranslateAnimation(
+            0.0f,
+            0.0f,
+            0.0f,
+            -150.0f
+        )
+        translateAnimation1.duration = 200
+
+        translateAnimation1.startOffset = 0
+
+        translateAnimation1.repeatCount = 0
+
+        translateAnimation1.interpolator = android.view.animation.LinearInterpolator()
+
+        val translateAnimation1r = TranslateAnimation(
+            0.0f,
+            0.0f,
+            -150.0f,
+            0.0f
+        )
+        translateAnimation1r.duration = 200
+
+        translateAnimation1r.startOffset = 0
+
+        translateAnimation1r.repeatCount = 0
+
+        translateAnimation1r.interpolator = android.view.animation.LinearInterpolator()
+
+        val animationSequence1 = AnimationSequence(imageView1!!, translateAnimation1, translateAnimation1r)
+
+        animationSequence1.start()
+
+        val imageView2 = chipImageMatrix[y2][x2]
+
+        val translateAnimation2 = TranslateAnimation(
+            0.0f,
+            0.0f,
+            0.0f,
+            150.0f
+        )
+        translateAnimation2.duration = 200
+
+        translateAnimation2.startOffset = 0
+
+        translateAnimation2.repeatCount = 0
+
+        translateAnimation2.interpolator = android.view.animation.LinearInterpolator()
+
+        val translateAnimation2r = TranslateAnimation(
+            0.0f,
+            0.0f,
+            150.0f,
+            0.0f
+        )
+        translateAnimation2r.duration = 200
+
+        translateAnimation2r.startOffset = 0
+
+        translateAnimation2r.repeatCount = 0
+
+        translateAnimation2r.interpolator = android.view.animation.LinearInterpolator()
+
+        val animationSequence2 = AnimationSequence(imageView2!!, translateAnimation2, translateAnimation2r)
+
+        animationSequence2.start()
+
+    }
+
+    fun startAnimationTopBottom(x1: Int, y1: Int, x2: Int, y2: Int)
+    {
+        val imageView1 = chipImageMatrix[y1][x1]
+
+        val translateAnimation1 = TranslateAnimation(
+            0.0f,
+            0.0f,
+            0.0f,
+            -150.0f
+        )
+        translateAnimation1.duration = 200
+
+        translateAnimation1.zAdjustment = Animation.ZORDER_TOP
+
+        translateAnimation1.startOffset = 0
+
+        translateAnimation1.repeatCount = 1
+
+        translateAnimation1.interpolator = android.view.animation.LinearInterpolator()
+
+        translateAnimation1.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                animation?.cancel()
+                swap(x1,y1, x2, y2)
+                updateImages()
+            }
+        })
+
+        imageView1?.startAnimation(translateAnimation1)
+
+        val imageView2 = chipImageMatrix[y2][x2]
+
+        val translateAnimation2 = TranslateAnimation(
+            0.0f,
+            0.0f,
+            0.0f,
+            150.0f
+        )
+        translateAnimation2.duration = 200
+
+        translateAnimation2.zAdjustment = Animation.ZORDER_TOP
+
+        translateAnimation2.startOffset = 0
+
+        translateAnimation2.repeatCount = 1
+
+        translateAnimation2.interpolator = android.view.animation.LinearInterpolator()
+
+        translateAnimation2.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                animation?.cancel()
+            }
+        })
+
+        imageView2?.startAnimation(translateAnimation2)
+
+    }
+
+    fun setListeners(){
+        for (y in 0 until rows) {
+            for (x in 0 until columns) {
+                val imageView = chipImageMatrix[y][x] ?: continue
+                  imageView.setOnClickListener {
+
+                      if(x==currentX+1 && y==currentY)
+                      {
+                          startAnimationLeftRightFail(x,y, currentX, currentY)
+                          currentX = -1
+                          currentY = -1
+                      }
+                      else if(x==currentX-1 && y==currentY)
+                      {
+                          startAnimationLeftRightFail(currentX, currentY, x, y)
+                          currentX = -1
+                          currentY = -1
+                      }
+                      else if(x==currentX && y==currentY+1)
+                      {
+                          startAnimationTopBottomFail(x, y, currentX, currentY)
+                          currentX = -1
+                          currentY = -1
+                      }
+                      else if(x==currentX && y==currentY-1)
+                      {
+                          startAnimationTopBottomFail(currentX, currentY, x, y)
+                          currentX = -1
+                          currentY = -1
+                      }
+                      else
+                      {
+                            currentX=x
+                            currentY=y
+                      }
+                      updateImages()
+                  }
+            }
+        }
+    }
 }
